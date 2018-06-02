e67b5c0170e3b46b8ce42a4e6483708e3a98be52
Lynsey Liu <lynseyliu@gmail.com>
2018-06-02 10:58:45 -0700

    Script to augment malignant data 4x

--- augment_data.py
+++ augment_data.py
@@ -7,7 +7,10 @@ for i in range(1, 4):
     for filename in os.listdir(root_dir):
         image = os.path.join(root_dir, filename)
         print(image)
-        image_to_rotate = ndimage.imread(image)
+        try:
+            image_to_rotate = ndimage.imread(image)
+        except:
+            continue
         rotated = ndimage.rotate(image_to_rotate, 45 * i)
-        fullpath = os.path.join(root_dir + str(i), 'rotated_' + filename)
+        fullpath = os.path.join(root_dir + str(i), 'rotated' + str(i) + '_' + filename)
         misc.imsave(fullpath, rotated)
