ec502004962fc437322856ecedf35fc4cdc4d9a5
sklezovich <sklezovich@users.noreply.github.com>
2018-06-02 22:14:00 -0700

    Adding plot_progress
    More comprehensive labels

--- plot_progress.py
+++ plot_progress.py
@@ -1,6 +1,9 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pdb
 import argparse
+import numpy as np
 
 
 def argParser():
@@ -14,8 +17,13 @@ def main():
 	train_accuracy=[]
 	test_accuracy=[]
 	train_loss=[]
+	bcount = 0
+	btotal = 0
+	mcount = 0
+	mtotal = 0
 	with open(args.file_name) as f:
 		for line in f:
+			
 			if 'Final Summary' in line:
 				train_loss.append(float(line[:-1].split(' ')[-1]))
 			elif 'Train Accuracy of the network' in line:
@@ -23,14 +31,14 @@ def main():
 			elif 'Test Accuracy of the network' in line:
 				test_accuracy.append(float(line[:-1].split(' ')[-2]))
 
+	plt.plot(train_accuracy, label= 'train_accuracy')
+	plt.plot(test_accuracy, label = 'test_accuracy')
+	plt.plot(train_loss, label = 'train_loss')
+	plt.xlabel('epochs')
+	plt.ylabel('accuracy')
+	plt.title('LazyNet Augmented')
+	plt.legend(loc = 'best')
+	plt.savefig('LazyNet-augmented-plot.png')
 	
-	plt.plot(train_accuracy)
-	plt.plot(test_accuracy)
-	plt.show()
-	plt.plot(train_loss)
-	plt.show()
-
-
-
 if __name__ == '__main__':
 	main()
